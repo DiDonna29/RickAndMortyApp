bcba83b13a2be693578b366655bf31d9054cd976
John Di Donna <139842867+DiDonna29@users.noreply.github.com>
2024-03-14 22:06:10 -0400

    comit 14-03-24

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -2,13 +2,13 @@ let myFavorites = [];
 
 const postFav = (req, res) => {
   myFavorites.push(req.body);
-  res.status(200).json(myFavorites);
+  return res.status(200).json(myFavorites);
 };
 
 const deleteFav = (req, res) => {
   const charId = req.params.id;
   myFavorites = myFavorites.filter(character => character.id !== +charId);
-  res.status(200).json(myFavorites);
+  return res.status(200).json(myFavorites);
 };
 
 module.exports = {
